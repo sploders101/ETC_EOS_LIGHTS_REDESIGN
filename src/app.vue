d80441b0dc4fa58b29bd5e64f1b5658dd0765c28
Shaun Keys <mariobuddy@gmail.com>
2018-09-15 14:31:37 -0600

    Replaced template with electron-forge-vue

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>Hello from {{text}}</h2>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data () {
+    return {
+       text: ''
+    }
+  }
+}
+</script>
+
+<script lang="scss">
+
+</script>
