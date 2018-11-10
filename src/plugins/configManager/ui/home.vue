2864018f547907b6d8147f384fb5ea96711f069e
Shaun Keys <mariobuddy@gmail.com>
2018-11-09 22:51:09 -0700

    Added home cards

---
+++ src/plugins/configManager/ui/home.vue
@@ -0,0 +1,41 @@
+<template>
+    <md-card md-with-hover>
+        <md-card-header>
+            Configuration Manager API
+        </md-card-header>
+        <md-card-content>
+            Plugin is initialized
+        </md-card-content>
+        <md-card-actions>
+            <md-button @click="showDialog = true;">More Info</md-button>
+        </md-card-actions>
+        <md-dialog :md-active.sync="showDialog">
+            <md-dialog-title>Details</md-dialog-title>
+            <p class="dialogContent">
+                This plugin provides a configuration manager to keep all plugins' configuration files in the same place.
+            </p>
+            <md-dialog-actions>
+                <md-button class="md-primary" @click="showDialog = false;">OK</md-button>
+            </md-dialog-actions>
+        </md-dialog>
+    </md-card>
+</template>
+
+<script lang="ts">
+    import Vue from '../../../ui/wrapper/vue';
+    import {VueConstructor} from 'vue';
+    export default Vue.extend({
+        data: function() {
+            return {
+                showDialog: false
+            }
+        }
+    });
+</script>
+
+<style lang="scss" scoped>
+    .dialogContent {
+        margin-left: 24px;
+        margin-right: 24px;
+    }
+</style>
