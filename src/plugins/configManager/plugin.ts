966922bf40794ee38298344d2f45a71c6662c71e
Shaun Keys <mariobuddy@gmail.com>
2018-11-10 09:57:05 -0700

    Made configManager preload and cache configs

--- src/plugins/configManager/plugin.ts
+++ src/plugins/configManager/plugin.ts
@@ -3,24 +3,37 @@ import fs = require("fs");
 import path = require("path");
 import { app } from 'electron';
 
-if(!fs.existsSync(app.getPath("userData"))) {
-    fs.mkdirSync(app.getPath("userData"));
-}
+let configs = new Map<string,object>();
 
 export default function init(msg:Messager) {
-    msg.on("/config/get",(configName:string, defaultConfig:object) => {
-        let configFile = path.join(app.getPath("userData"), configName + ".json");
-        if(fs.existsSync(configFile)) {
-            fs.readFile(configFile,(err,data) => {
-                if(err) throw err;
-                msg.send("/config/get/"+configName,JSON.parse(data.toString()));
-            });
-        } else {
-            msg.send("/config/get/"+configName,defaultConfig);
+
+    let files = fs.readdirSync(app.getPath("userData"));
+    files.forEach((file) => {
+        let matches = (/^pluginconfig_(.*).json$/g).exec(file);
+        if(matches) {
+            configs.set(matches[1],
+                JSON.parse(
+                    fs.readFileSync(
+                        path.join(
+                            app.getPath("userData"),
+                            file
+                        )
+                    ).toString()
+                )
+            );
         }
     });
+
+    msg.on("/home/mounted", () => {
+        msg.send("/home/add", `${__dirname}/ui/home`);
+    });
+
+    msg.on("/config/get",(configName:string, defaultConfig:object) => {
+        msg.send(`/config/get/${configName}`, (configs.has(configName)) ? (configs.get(configName)) : (defaultConfig));
+    });
     msg.on("/config/set",(configName:string, config:object) => {
-        let configFile = path.join(app.getPath("userData"), configName + ".json");
+        configs.set(configName, config);
+        let configFile = path.join(app.getPath("userData"), "pluginconfig_"+configName + ".json");
         fs.writeFile(configFile,JSON.stringify(config,null,4)+"\n",(err) => {
             if(err) throw err;
             msg.send("/config/set/"+configName);
