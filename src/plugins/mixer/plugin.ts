c27b61e47e5c4fa8c1dfccfca99c78b166f438d9
Shaun Keys <mariobuddy@gmail.com>
2018-11-10 23:59:14 -0700

    Fixed animations not linked to fxEngine, and added fx state transitions

--- src/plugins/mixer/plugin.ts
+++ src/plugins/mixer/plugin.ts
@@ -2,21 +2,44 @@ import { Messager } from '../loader';
 
 let subLayers: string[] = [];
 let subMixes = new Map<string, Map<number, number>>();
+let layerWeights = new Map<string,number>();
 
 export default function init(msg:Messager) {
     msg.on("/board/mixer", function(command:string, identifier:string, submaster:number, val:number) {
-        if (!subMixes.has(identifier)) subMixes.set(identifier, new Map<number, number>());
+        if (!subMixes.has(identifier)) {
+            subMixes.set(identifier, new Map<number, number>());
+            layerWeights.set(identifier, 0);
+            msg.emit("/anime/timeline/new", {
+                name: "mixer:"+identifier,
+                common: {
+                    targets: {
+                        weightVal: 0
+                    },
+                    easing: 'linear',
+                    duration: 200,
+                },
+                objects: [
+                    {
+                        weightVal: 1
+                    }
+                ]
+            });
+            msg.on(`/anime/mixer:${identifier}/event/update`,(targets: any) => {
+                layerWeights.set(identifier, targets.weightVal);
+            });
+        }
         switch (command) {
             case "enable":
                 if (subLayers.indexOf(identifier) >= 0) {
                     subLayers.splice(subLayers.indexOf(identifier), 1);
                 }
                 subLayers.push(identifier);
-                // this.updateLayerMix(identifier);
+                msg.emit(`/anime/mixer:${identifier}/setDirection`, false, true);
                 break;
             case "disable":
                 updateLayerMix(identifier);
-                subLayers.splice(subLayers.indexOf(identifier), 1);
+                // subLayers.splice(subLayers.indexOf(identifier), 1);
+                msg.emit(`/anime/mixer:${identifier}/setDirection`, true, true);
                 break;
             case "remove":
                 subLayers.splice(subLayers.indexOf(identifier), 1);
@@ -40,8 +63,8 @@ export default function init(msg:Messager) {
         let channel: number = 0;
         subLayers.forEach((layer) => {
             // Max blend mode
-            if (subMixes.get(layer)!.has(sub) && channel < subMixes.get(layer)!.get(sub!)!) {
-                channel = subMixes.get(layer)!.get(sub)!;
+            if (subMixes.get(layer)!.has(sub) && channel < (subMixes.get(layer)!.get(sub!)! * layerWeights.get(layer)!)) {
+                channel = (subMixes.get(layer)!.get(sub)!) * layerWeights.get(layer)!;
             }
         });
         msg.emit("/board/command", "sendSub", sub, channel);
